05c3389c6098b304fef01732d03a5567de4b51a0
donk7413 <romainchevallier74@gmail.com>
2023-01-25 15:12:13 +0100

    Update init.lua

--- bin/x64/plugins/cyber_engine_tweaks/mods/zzzzzz_cyberscript_amm_compatibility/init.lua
+++ bin/x64/plugins/cyber_engine_tweaks/mods/zzzzzz_cyberscript_amm_compatibility/init.lua
@@ -1,10 +1,28 @@
 registerForEvent("onTweak", function()
 	
-	
-	
-	
+		
 print("CyberScript : AMM Compatibility Patch")
 		TweakDB:SetFlat("PreventionSystem.setup.totalEntitiesLimit", 999999)
 	
+
+
+	
+end)
+
+registerForEvent("onInit", function()
+	
+
+Override("ShardsNestedListDataView", "SortItems", function (this, compareBuilder, left, right, wrapped)
+		print("nested")
+			local leftData = ShardEntryData.new()
+			local rightData = ShardEntryData.new()
+			leftData = left.data
+			rightData = right.data
+			if IsDefined(leftData) and IsDefined(rightData) then
+			compareBuilder:BoolTrue(leftData.isNew, rightData.isNew):GameTimeDesc(leftData.timeStamp, rightData.timeStamp)
+			end
+	end)
+
+
 	
 end)
